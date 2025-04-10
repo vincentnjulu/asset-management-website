a6c0f93f4a0a7edeb471b8220d8ff3fd1e569359
Vincent.N <vincentnjulu1@gmail.com>
2025-04-10 09:32:04 +0300

    Add files via upload

---
+++ contact.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const { sendMessage } = require('../controllers/contactController');
+
+router.post('/', sendMessage);
+
+module.exports = router;
